044a3d5c32bcedf0a223a1a9cac854a2826722e0
Chris Hartley <6811393+chartleyit@users.noreply.github.com>
2025-06-02 08:21:01 -0400

    minor updates to default cmd/root

--- cmd/root.go
+++ cmd/root.go
@@ -30,17 +30,14 @@ import (
 )
 
 var cfgFile string
+var format string
+var file string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "todo-cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Create and manage todo lists",
+	Long:  `Provide a longer description later`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -63,6 +60,8 @@ func init() {
 	// will be global for your application.
 
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.todo-cli.yaml)")
+	rootCmd.PersistentFlags().StringVar(&format, "json", "", "todo list format (default csv)")
+	rootCmd.PersistentFlags().StringVar(&file, "file", "todo.csv", "name of todo file")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
